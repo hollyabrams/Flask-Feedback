991f2edd9de7da9d58c94c981c14509e62ab043d
Holly <holly.d.abrams@gmail.com>
2022-10-19 17:50:49 -0400

    First commit - Implement authentication and authorization in flask with bcrypt

---
+++ forms.py
@@ -0,0 +1,63 @@
+"""Forms for Flask Feedback"""
+
+from wtforms import StringField, PasswordField
+from wtforms.validators import InputRequired, Length
+from flask_wtf import FlaskForm
+
+
+class LoginForm(FlaskForm):
+    """Login form."""
+
+    username = StringField(
+        "Username",
+        validators=[InputRequired(), Length(min=1, max=20)],
+    )
+    password = PasswordField(
+        "Password",
+        validators=[InputRequired(), Length(min=6, max=55)],
+    )
+
+###################################################################################################
+
+class RegisterForm(FlaskForm):
+    """User registration form."""
+
+    username = StringField(
+        "Username",
+        validators=[InputRequired(), Length(min=1, max=20)],
+    )
+    password = PasswordField(
+        "Password",
+        validators=[InputRequired(), Length(min=6, max=55)],
+    )
+    email = StringField(
+        "Email",
+        validators=[InputRequired(), Length(max=50)],
+    )
+    first_name = StringField(
+        "First Name",
+        validators=[InputRequired(), Length(max=30)],
+    )
+    last_name = StringField(
+        "Last Name",
+        validators=[InputRequired(), Length(max=30)],
+    )
+
+###################################################################################################
+
+class FeedbackForm(FlaskForm):
+    """Add feedback form."""
+
+    title = StringField(
+        "Title",
+        validators=[InputRequired(), Length(max=100)],
+    )
+    content = StringField(
+        "Content",
+        validators=[InputRequired()],
+    )
+
+###################################################################################################
+
+class DeleteForm(FlaskForm):
+    """Delete form -- this form is intentionally blank."""
